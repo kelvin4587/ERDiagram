8405e5dd2ac4956ca1013785ae38cb7014d0a3ea
Kelvin Wang <kelvin4587@hotmail.com>
2017-05-01 00:12:26 +0800

    ER图第一次提交

---
+++ README.md
@@ -0,0 +1,1 @@
+jsPlumb ERDiagram demo
---
+++ js/json/metadata.js
@@ -0,0 +1,149 @@
+/**
+ * 元数据
+ */
+var metadata = [
+    {
+        "Employee": {
+            "name": "职员",
+            "index": "Employee",
+            "type": "Employee",
+            "properties": {
+                "id": {
+                    "des": "id"
+                },
+                "name": {
+                    "des": "姓名"
+                },
+                "title": {
+                    "des": "职务"
+                },
+                "phone": {
+                    "des": "电话"
+                },
+                "email": {
+                    "des": "邮箱"
+                },
+                "companyId": {
+                    "des": "分公司ID"
+                }
+            }
+        }
+    },
+    {
+        "BeiJin": {
+            "name": "北京分公司",
+            "index": "BeiJin",
+            "type": "BeiJin",
+            "properties": {
+                "id": {
+                    "des": "id"
+                },
+                "description": {
+                    "des": "公司描述"
+                },
+                "employeeNumber": {
+                    "des": "员工数量"
+                },
+                "registeredCapital": {
+                    "des": "注册资本"
+                },
+                "address": {
+                    "des": "地址"
+                },
+                "telephone": {
+                    "des": "电话"
+                },
+                "companyGroupID": {
+                    "des": "所属集团"
+                }
+            }
+        }
+    },
+    {
+        "Guangzhou": {
+            "name": "广州分公司",
+            "index": "Guangzhou",
+            "type": "Guangzhou",
+            "properties": {
+                "id": {
+                    "des": "id"
+                },
+                "description": {
+                    "des": "公司描述"
+                },
+                "employeeNumber": {
+                    "des": "员工数量"
+                },
+                "registeredCapital": {
+                    "des": "注册资本"
+                },
+                "address": {
+                    "des": "地址"
+                },
+                "telephone": {
+                    "des": "电话"
+                },
+                "companyGroupID": {
+                    "des": "所属集团"
+                }
+            }
+        }
+    },
+    {
+        "TianJin": {
+            "name": "天津分公司",
+            "index": "TianJin",
+            "type": "TianJin",
+            "properties": {
+                "id": {
+                    "des": "id"
+                },
+                "description": {
+                    "des": "公司描述"
+                },
+                "employeeNumber": {
+                    "des": "员工数量"
+                },
+                "registeredCapital": {
+                    "des": "注册资本"
+                },
+                "address": {
+                    "des": "地址"
+                },
+                "telephone": {
+                    "des": "电话"
+                },
+                "companyGroupID": {
+                    "des": "所属集团"
+                }
+            }
+        }
+    },
+    {
+        "CompanyGroup": {
+            "name": "集团",
+            "index": "CompanyGroup",
+            "type": "CompanyGroup",
+            "properties": {
+                "id": {
+                    "des": "id"
+                },
+                "description": {
+                    "des": "集团描述"
+                },
+                "employeeNumber": {
+                    "des": "员工数量"
+                },
+                "registeredCapital": {
+                    "des": "注册资本"
+                },
+                "address": {
+                    "des": "地址"
+                },
+                "telephone": {
+                    "des": "电话"
+                }
+            }
+        }
+    }
+];
